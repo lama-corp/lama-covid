d69f48938a63cde0f2ad2bfa9d534a7abd90e12c
Romain Poussier <romain.gm.poussier@gmail.com>
2021-07-17 15:55:41 +0200

    🔍️ Adding meta data & socials

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,17 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lama-covid',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    title: 'Covid Prochaine Vague',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
     ],
+    htmlAttrs: {
+      lang: 'fr',
+    },
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
