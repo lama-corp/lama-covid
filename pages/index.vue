53f1c7588454a968b70630b6bb00e6df1614b7cb
Romain Poussier <romain.gm.poussier@gmail.com>
2021-07-20 20:37:37 +0200

    ✨ Line chart and a bar chart

--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,63 @@
       </h1>
     </div>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
       <div class="px-4 py-8 sm:px-0">
         <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
-          <Tutorial />
+          <div class="relative min-h-screen bg-gray-100 p-5">
+            <h1 class="text-3xl font-bold leading-tight text-gray-900">
+              Ceci est un test
+            </h1>
+            <BarChart
+              :data="barChartData"
+              :options="{ maintainAspectRatio: false }"
+            />
+            <LineChart
+              :data="{
+                labels: ['Janv', 'Fev', 'Mars', 'Avr', 'Mai', 'Juin', 'Juill'],
+                datasets: [
+                  {
+                    label: 'My First DS',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1,
+                  },
+                ],
+              }"
+              :options="{
+                type: 'line',
+              }"
+            />
+          </div>
         </div>
       </div>
-      <!-- /End replace -->
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
+  async asyncData({ $axios }) {
+    const stats = await $axios.$get(
+      'https://api.github.com/repos/nuxt/nuxt.js/stats/commit_activity'
+    )
+    return {
+      barChartData: {
+        labels: stats.map((stat) =>
+          moment(stat.week * 1000).format('GGGG[-W]WW')
+        ),
+        datasets: [
+          {
+            label: 'Nuxt Commit Activity',
+            backgroundColor: '#41B38A',
+            data: stats.map((stat) => stat.total),
+          },
+        ],
+      },
+    }
+  },
   data() {
     return {
       socialShare: {
